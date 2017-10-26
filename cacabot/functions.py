94455426e8c7785fe9423469f1a2d06eddca0aa9
WHWH10 <eyet010@gmail.com>
2017-10-26 18:05:37 +0900

    Update functions.py

--- cacabot/functions.py
+++ cacabot/functions.py
@@ -26,7 +26,7 @@ def car_type1(car_type):
     eun = {'뉴 i30(PD)' : 5506, '뉴 i40' : 5510, '그랜저 하이브리드(HG)' : 5309, '그랜저 하이브리드(IG)' : 5489, '그랜저(IG)' : 5384, '뉴 맥스크루즈' : 5386, '벨로스터': 4969, '그랜드 스타렉스' : 5344, '싼타페 더 프라임' : 5438, '쏘나타 뉴 라이즈' : 5456, '쏘나타 뉴 라이즈 플러그인 하이브리드' : 5555, '쏘나타 뉴 라이즈 하이브리드' : 5520, '쏠라티' : 5401, '뉴 아반떼(AD)' : 5462}
     name = eun.get(car_type)
 
-    jsonp_string = requests.get('http://www.carisyou.com/estimate/ajax/TrimList.do?carClassNbr=' + str(name)).text
+    jsonp_string = requests.get('http://www.carisyou.com/' + str(name)).text
     json_string = jsonp_string.replace('_(', '').replace(');', '')
 
     meta = json.loads(jsonp_string)
@@ -49,7 +49,7 @@ def Car_Price(car_price):
     eun = {'1.6 GDi Style':53089,'1.6 GDi Value Plus':53091, '1.6 GDi Smart':53092, '1.6 GDi Modern':53093, '1.6 GDi Premium':53094, '1.6 VGT Style':53096, '1.6 VGT Smart':53098, '1.6 VGT Smart Special':53099, '1.6 VGT Premium':53100, '1.6 T-GDi SPORT':53101, '1.6 T-GDi SPORT 7DCT':53102, '1.6 T-GDi SPORT Extreme Selection':53103, '1.6 T-GDi SPORT Original':53104 }
     name = eun.get(car_price)
 
-    jsonp_string = requests.get('http://www.carisyou.com/estimate/ajax/Compute.do?estimateType=1&useTerm=36&carGradeNbr=' + str(name)).text
+    jsonp_string = requests.get('http://www.carisyou.com/' + str(name)).text
     json_string = jsonp_string.replace('_(', '').replace(');', '')
 
     meta = json.loads(jsonp_string)
@@ -69,7 +69,7 @@ def Car_PriceUrl(car_price):
     eun = {'1.6 GDi Style':53089,'1.6 GDi Value Plus':53091, '1.6 GDi Smart':53092, '1.6 GDi Modern':53093, '1.6 GDi Premium':53094, '1.6 VGT Style':53096, '1.6 VGT Smart':53098, '1.6 VGT Smart Special':53099, '1.6 VGT Premium':53100, '1.6 T-GDi SPORT':53101, '1.6 T-GDi SPORT 7DCT':53102, '1.6 T-GDi SPORT Extreme Selection':53103, '1.6 T-GDi SPORT Original':53104 }
     name = eun.get(car_price)
 
-    url = 'http://www.carisyou.com/estimate/5462?brandNbr=904&carGradeNbr=' + str(name)
+    url = 'http://www.carisyou.com/' + str(name)
 
     return url
 
@@ -101,7 +101,7 @@ def car_brandname(car_brand):
     eun = {'현대자동차' : 904, '기아자동차' : 1193, 'BMW' : 212, '폭스바겐' : 2331 }
     name = eun.get(car_brand)
 
-    jsonp_string = requests.get('http://www.carisyou.com/estimate/ajax/ModelList.do?brandNbr=' + str(name)).text
+    jsonp_string = requests.get('http://www.carisyou.com/' + str(name)).text
     json_string = jsonp_string.replace('_(', '').replace(');', '')
 
     meta = json.loads(jsonp_string)
